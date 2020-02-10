612cc2f8fe0bd2895c2a361d0ecbc865bd8b3623
Nino <dolce.antonio@gmail.com>
2020-02-10 07:01:48 +0100

    s3 input

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hel 2lo world",
+            "message": "hel2lo world",
             # "location": ip.text.replace("\n", "")
         }),
     }
